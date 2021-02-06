22e79380e077f0adafee3e5dbf3109568ac8c1c6
“VijayMeena701” <govardhanmeena701@gmail.com>
2021-02-06 19:39:34 +0530

    Updated Component NavigationBar.js from Functional to Class Component

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {React, Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import PropTypes from "prop-types"
 import MenuIcon from '@material-ui/icons/Menu';
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = (theme) => ({
   root: {
     flexGrow: 1,
   },
@@ -17,24 +18,30 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
-}));
+});
 
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+class NavigationBar extends Component {
+    render(){
+        const {classes} = this.props;
+        return(
+        <div className={classes.root}>
+            <AppBar position="static" style={{backgroundColor: 'transparent'}}>
+                <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                    News
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+        );
+    }
 }
+NavigationBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(useStyles)(NavigationBar);
